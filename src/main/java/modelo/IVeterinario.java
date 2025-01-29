4725176fae4853f34bca32bd2ffd1bfa05cc84e2
Lucia <ludegu888@gmail.com>
2025-01-29 14:20:27 +0100

    Interfaces

---
+++ src/main/java/modelo/IVeterinario.java
@@ -0,0 +1,8 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IVeterinario {
+
+}
---
+++ src/main/java/modelo/VeterinarioDTO.java
@@ -0,0 +1,12 @@
+
+package modelo;
+
+public class VeterinarioDTO {
+    private int idVet;
+    private String nombreVet;
+    private String nifVet;
+    private String direccion;
+    private String telefono;
+    private String email;
+    
+}
